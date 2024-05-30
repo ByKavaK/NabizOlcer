c76a546bf5d1eb7f6ca343fe14a7bf05ab1c844b
Yasin Kavak <ysnkvk2002@gmail.com>
2024-05-30 21:18:56 +0300

    duzeltme
    duzeltme

--- led_ekran_nabiz.ino
+++ led_ekran_nabiz.ino
@@ -1,17 +1,21 @@
-
 #define USE_ARDUINO_INTERRUPTS true
 #include <PulseSensorPlayground.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <SoftwareSerial.h>
 
 #define SCREEN_WIDTH 128 
 #define SCREEN_HEIGHT 64 
 #define OLED_RESET     4 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-
+String agAd = "";
+String sifre = "";
+String ip = "";
+int rxPin = 10;
+int txPin = 11;
 int nabiz;
 
 const int PulseWire = 0; 
@@ -19,14 +23,37 @@ const int LED13 = 13;
 int Threshold = 510;
 PulseSensorPlayground pulseSensor;
 
+SoftwareSerial esp(rxPin, txPin);
+
 void setup() {
   
   Serial.begin(9600);
 
+  Serial.println("Haberleşme Başladı.");
+  esp.begin(115200);
+  esp.println("AT");
+  Serial.println("AT Geldi.");
+  while(!esp.find("OK")){
+    esp.println("AT");
+    Serial.println("ESP Bulunamadı.");
+  }
+  Serial.println("OK Komutu Alındı");
+  esp.println("AT+CWMODE=1");                          
+  while(!esp.find("OK")){                                   
+    esp.println("AT+CWMODE=1");
+    Serial.println("Ayar Yapılıyor....");
+  }
+  Serial.println("Client olarak ayarlandı");
+  Serial.println("Aga Baglaniliyor...");
+  esp.println("AT+CWJAP=\""+agAd+"\",\""+sifre+"\"");    
+  while(!esp.find("OK"));                                     
+  Serial.println("Aga Baglandi.");
+  delay(1000);
+
   pulseSensor.analogInput(PulseWire);
   pulseSensor.blinkOnPulse(LED13);
   pulseSensor.setThreshold(Threshold);
-  
+
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); 
@@ -53,8 +80,30 @@ void setup() {
 }
 
 void loop() {
+  esp.println("AT+CIPSTART=\"TCP\",\""+ip+"\",80");           
+  if(esp.find("Error")){                                      
+    Serial.println("AT+CIPSTART Error");
+  }
+
   if (pulseSensor.sawStartOfBeat()) {
     nabiz = pulseSensor.getBeatsPerMinute();
+
+    String veri = "GET https://api.thingspeak.com/update?api_key=";                                     
+    veri += "&field1=";
+    veri += String(nabiz);                                      
+    veri += "\r\n\r\n"; 
+    esp.print("AT+CIPSEND=");                                   
+    esp.println(veri.length()+2);
+    delay(2000);
+    if(esp.find(">")){                                          
+      esp.print(veri);                                         
+      Serial.println(veri);
+      Serial.println("Veri gonderildi.");
+      delay(1000);
+    }
+
+    Serial.println("Baglantı Kapatildi.");
+    delay(1000);
 
     display.clearDisplay();
     display.setTextSize(2);
